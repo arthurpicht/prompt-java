f982763e692d30dde3b02bfcbedc73af87e6d445
Arthur Picht <picht@arthurpicht.de>
2021-05-08 18:14:44 +0200

    inline code documentation: escaping and wrapping ansi codes

--- src/main/java/de/arthurpicht/prompt/Prompt.java
+++ src/main/java/de/arthurpicht/prompt/Prompt.java
@@ -12,6 +12,13 @@ public class Prompt {
         String prompt;
         try {
             prompt = Infrastructure.get() + UserAtHost.get() + ":" + WorkingDirChunk.get() + Branch.get() + "$ ";
+
+            // escape trailing $ to prevent unexpected behavior (jumping back cursor), see:
+            // https://stackoverflow.com/questions/37424743/sometimes-cursor-jumps-to-start-of-line-in-bash-prompt
+
+            // wrapping correctly:
+            // https://unix.stackexchange.com/questions/105958/terminal-prompt-not-wrapping-correctly
+
         } catch (Exception e) {
             prompt = "[prompt error]$ ";
         }
